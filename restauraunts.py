cdbfbb5898cf57116725fc844c6644f992539ff4
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-18 23:45:31 +0100

    stuff

--- restauraunts.py
+++ restauraunts.py
@@ -4,3 +4,35 @@ from flask_login import current_user, login_required
 from .models import restauraunt
 
 restauraunts = Blueprint("restauraunts", __name__)
+
+@restauraunts.route("/api/ratings/restaurants/add", methods=["POST"])
+def api_add_restuaraunt():
+    try: 
+        if request.headers['Content-Type'] == 'restaurauntslication/json':
+            data = request.get_json()
+            name = data["name"]
+            overall_rating = float(data["rating"])
+            x = restauraunt(name=name, overall_rating=overall_rating)
+            db.session.add(x)
+            db.session.commit()
+            return jsonify(msg="worked")
+        else:
+            return jsonify(error="wrong stuff")
+    except:
+        return jsonify(error="error!")
+
+@restauraunts.route("/api/restauraunt/add")
+def api_2_restauraunt_add():
+    name = request.args.get("name")
+    overall_rating = request.args.get("rating")
+    x = restauraunt(name=name, overall_rating=overall_rating)
+    db.session.add(x)
+    db.session.commit()
+    return jsonify(message="worked")
+
+@restauraunts.route("/api/restuaraunt/delete")
+def api2_restauraunt_delete():
+    id = request.args.get("id")
+    restaurauntx = db.session.query(restauraunt).filter(restauraunt.id == id).first()
+    db.session.delete(restaurauntx)
+    db.session.commit()
