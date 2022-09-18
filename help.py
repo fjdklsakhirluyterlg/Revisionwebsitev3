d5f90fb5e91529d18d3b179b60a943d4086e97b8
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-18 23:46:42 +0100

    stuff

--- help.py
+++ help.py
@@ -24,3 +24,45 @@ def update_help(id):
     x = db.session.query(Help).filter(Help.id == id).first()
     db.session.delete(x)
     db.session.commit()
+
+@help.route("/api/help/add")
+def api_add_help():
+    try:
+        data = request.get_json()
+        subject = data["subject"]
+        # subject = "test"
+        topic = data["topic"]
+        # topic = "test"
+        question = data["question"]
+        # question = "test"
+        awnser = data["awnser"]
+        # awnser = "test"
+        x = Help(subject=subject, topic=topic, question=question, awnser=awnser)
+        db.session.add(x)
+        db.session.commit()
+        return "added!"
+    except:
+        return jsonify(error="error!")
+
+@help.route("/api/help/view")
+def api_view_help():
+    subject = request.args.get("subject", default="none")
+    if subject != "none":
+        y = db.session.query(Help).filter(Help.subject == subject).all()
+        return jsonify(message=y)
+    else:
+        y = db.session.query(Help).all()
+    topic = request.args.get("topic", default="none")
+    if topic != "none":
+        a = db.session.query(Help).filter(Help.topic == topic).all()
+    else:
+        a = db.session.query(Help.subject).all()
+    question = request.args.get("question", default="none")
+    if question != "none":
+        b = db.session.query(Help.question).filter(Help.question == question).first()
+        c = b.awnser
+    else:
+        b = db.session.query(Help.topic).all()
+        c = db.session.query(Help.question).all()
+    
+    return jsonify(subject=f"y", topic=f"a", question=f"b", awnser=f"c")
