bc63b3acf38d6a73247625875b43b6765afb539c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-19 11:33:04 +0100

    stuff

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,6 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_socketio import SocketIO, send
-from .models import User
 
 def create_app():
     app = Flask(__name__)
