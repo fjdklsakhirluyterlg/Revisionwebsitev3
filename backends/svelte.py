99234ab0f10060554d38f8f821b2bba5643c4060
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-21 22:04:00 +0100

    stuff

--- backends/svelte.py
+++ backends/svelte.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 
-svelte = Blueprint()
+svelte = Blueprint("svelte", __name__)
+
+@svelte.route("/<path:path>")
+def base(path):
+    return send_from_directory('client/public', path)
+
+@svelte.route("/f/svelte/i")
+def maybe_it_returns_the_index():
+    return send_from_directory('client/public', 'index.html')
