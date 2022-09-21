0769ffca46cd1f0b58eb8e4dd0a0bae51f75d80a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-21 21:51:20 +0100

    stuff

---
+++ backends/stream.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
+
+# @app.route("/stream")
+# def stream():
+#     video = cv2.VideoCapture(0)
+#     def generator(webcam):
+#         while True:
+#             success, image = video.read()
+#             if success:
+#                 ret, jpeg = cv2.imencode('.jpg', image)
+#                 frame = jpeg.tobytes()
+#                 yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    
+#     return Response(generator(video),mimetype='multipart/x-mixed-replace; boundary=frame')
