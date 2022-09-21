4152c004d0d8e66a780cd96b7edce1cbd1e9dabc
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-21 21:33:12 +0100

    stuff

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from backends import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5060, host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5060))
+    app.run(debug=True, port=port, host="0.0.0.0")
