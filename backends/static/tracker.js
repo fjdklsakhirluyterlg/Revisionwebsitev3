4746c2aa38823b593e5d80e7c1ead717dcbc8b2b
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-22 20:16:34 +0100

    stuff

--- backends/static/tracker.js
+++ backends/static/tracker.js
@@ -3,4 +3,14 @@ document.addEventListener('mousemove', (e) => {
     const mousey = e.clientY
 
     const tracker = document.getElementById("tracker")
+    const rekt = tracker.getBoundingClientRect()
+    const trackerx = rekt.left + rekt.width / 2
+    const trackery = rekt.top + rekt.height / 2
 })
+
+function angle(cx, cy, ex, ey){
+    const dy = ey - cy
+    const dx = ex - cx
+    const rad = Math.atan2(dy, dx)
+    const dg = rad * 180 / Math.PI
+}
