014990706beea30c0ab1f83928d118e95d5e5a9d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-22 16:27:44 +0100

    stuff

--- backends/ide.py
+++ backends/ide.py
@@ -17,7 +17,7 @@ def python_ide():
         x = request.form.get("script")
         num = random.randint(0, 100000)
         path = f"./programs/file{num}.py"
-        with open(path, "w+") as file:
+        with open(path, "a+") as file:
             file.write(x)
         
         command = f'python {path}'
