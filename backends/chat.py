31cc508adf500457358b501589277cef05730265
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-23 22:01:08 +0100

    stuff

--- backends/chat.py
+++ backends/chat.py
@@ -63,9 +63,9 @@ def chat_with_other_users(id):
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
-
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
     
 @login_required
