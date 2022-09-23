c61e25f54b1d92840d6cb5dfee0bd72a56bdfbf7
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-23 21:53:34 +0100

    stuff

--- backends/static/chat.js
+++ backends/static/chat.js
@@ -6,8 +6,10 @@ socket.emit( 'my event', {
 } )
 var form = $( 'form' ).on( 'submit', function( e ) {
     e.preventDefault()
-    let user_input = $( 'input.text' ).val()
+    let user_name = $( 'input.username' ).val()
+    let user_input = $( 'input.message' ).val()
     socket.emit( 'my event', {
+    user_name : user_name,
     message : user_input
     } )
     $( 'input.message' ).val( '' ).focus()
@@ -17,6 +19,6 @@ socket.on( 'my response', function( msg ) {
 console.log( msg )
 if( typeof msg.user_name !== 'undefined' ) {
     $( 'h3' ).remove()
-    $( 'div.message_holder' ).append( '<div>'+msg.message+'</div>' )
+    $( 'div.message_holder' ).append( '<div><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>' )
 }
 })
