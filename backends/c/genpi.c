5ccbebd644af09e3971bb324e56258fd77f5707d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-24 21:22:09 +0100

    stuff

---
+++ backends/c/genpi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+float generateπ_from_random(int n){
+    int ins = 0;
+    int outs = 0;
+    for(int i = 0; i < n; i++){
+        srand((unsigned) time(0));
+        int x = rand() % 2;
+        int y = rand() % 2;
+        float distance = pow(x, 2) + pow(x, 2);
+        if (distance <= 1){
+            ins = ins + 1;
+        } else {
+            outs = outs + 1;
+        }
+    }
+
+    return 4*ins/outs;
+}
