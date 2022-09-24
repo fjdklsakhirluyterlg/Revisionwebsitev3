f22e22f75906efc219a635d5da5694d48dee6d5e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-24 21:28:31 +0100

    stuff

--- backends/c.py
+++ backends/c.py
@@ -7,4 +7,9 @@ loc = f"{dir}/c/square.so"
 
 functions = CDLL(loc)
 
+loc2 = f"{dir}/c/genpi.so"
+
+functions2 = CDLL(loc2)
+
 print(functions.square(10))
+print(functions2.generateπ_from_random(1000))
