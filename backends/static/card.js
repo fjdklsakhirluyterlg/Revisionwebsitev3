d88d630d9a3468928bffcf12c685c9cfe5d01950
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-25 19:49:28 +0100

    stuff

--- backends/static/card.js
+++ backends/static/card.js
@@ -23,17 +23,10 @@ function addcardform(){
     // document.body.appendChild(clon);
 }
 
-function zip(arrays) {
-    return arrays[0].map(function(_,i){
-        return arrays.map(function(array){return array[i]})
-    });
-}
-
 function addcards(){
-    const zip = (a, b) => a.map((k, i) => [k, b[i]]);
     var front = document.getElementsByClassName("front")
     var back = document.getElementsByClassName("back")
-    var cards = zip(front, back)
+    var cards = front.map(function(front, i){return [front, back[i]]})
     console.log(cards)
     // cards.forEach(element => {
     //     cardfetch(element[0].value, element[1].value)
