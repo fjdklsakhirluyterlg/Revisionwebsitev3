fb2053452708a004710cbe9c2e7013131e517dca
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-25 08:53:12 +0100

    stuff

--- backends/c.py
+++ backends/c.py
@@ -2,7 +2,7 @@ from ctypes import *
 import os
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 
-c = Blueprint()
+c = Blueprint("c", __name__)
 
 dir = os.getcwd()
 
