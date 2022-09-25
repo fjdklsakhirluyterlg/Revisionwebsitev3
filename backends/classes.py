3e1549aaeea4f886fcf67a336f09532a5ccad5dd
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-25 08:35:39 +0100

    stuff

--- backends/classes.py
+++ backends/classes.py
@@ -14,3 +14,24 @@ class Position:
         h = (sin((phi_2 - phi_1) / 2)**2
              + cos(phi_1) * cos(phi_2) * sin((lam_2 - lam_1) / 2)**2)
         return 2 * r * asin(sqrt(h))
+
+
+class bankuser:
+    user = {}
+    bankrupt = []
+    def add_user(self, name, money, account):
+        self.user[name] = [money, account]
+    def debers(self):
+        for name in self.user:
+            if self.user[name][0] <= 0:
+                self.bankrupt.append(name)
+    def interest(self):
+        for name in self.user:
+            if self.user[name][1] == "Gold":
+                self.user[name][0] = (self.user[name][0])*1.08
+            elif self.user[name][1] == "Silver":
+                self.user[name][0] = (self.user[name][0])*1.06
+            elif self.user[name][1] == "Bronze":
+                self.user[name][0] = (self.user[name][0])*1.04
+            elif self.user[name][1] == "Standard":
+                self.user[name][0] = (self.user[name][0])*1.02
