43a89ffc0897dd3ff5a0fcb1d5461cf991ab3d30
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-25 08:45:46 +0100

    stuff

--- backends/apix.py
+++ backends/apix.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, Api
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 import random
-from .functions import get_top_bbc_actual, get_top_bbc_links, get_top_bbc_news, get_top_bbc_content
+from .functions import get_top_bbc_actual, get_top_bbc_links, get_top_bbc_news, get_top_bbc_content, get_ecenomic_stuff
 
 class Randomz(Resource):
     def get(self):
@@ -32,3 +32,12 @@ class top_bbc_news(Resource):
         act = get_top_bbc_actual()
         res_dct = {i + 1: {'headline': lst[i], 'link': f"https://www.bbc.co.uk{link[i]}", 'info': cont[i], 'content': act[i], 'length': f"{len(act[i].split())} words"} for i in range(0, len(lst))}
         return res_dct
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'there'}
+
+class GBPTOEUR(Resource):
+    def get(self):
+        x = get_ecenomic_stuff()[0]
+        return {'GBP': x}
