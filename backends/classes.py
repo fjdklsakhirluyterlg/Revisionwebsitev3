59ed6dff8de8f62d3be36811ba2df8edc189e1a9
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-25 08:36:34 +0100

    stuff

--- backends/classes.py
+++ backends/classes.py
@@ -54,3 +54,49 @@ class Bank():
                     yield f"£{money} is not avaible to you, please withdraw a max of £{newmoneylimit}"
                 else:
                     yield f"£{money} is not avaible to you, please withdraw a max of £{moneylimit}"
+
+class Vector:
+
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
+
+  def magnitude(self):
+
+    return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+  def __add__(self, V):
+
+    return Vector(self.x + V.x, self.y + V.y, self.z + V.z)
+
+  # Method to subtract 2 Vectors
+  def __sub__(self, V):
+
+    return Vector(self.x - V.x, self.y - V.y, self.z - V.z)
+
+  # Method to calculate the dot product of two Vectors
+  def __xor__(self, V):
+
+    return self.x * V.x + self.y * V.y + self.z * V.z
+
+  # Method to calculate the cross product of 2 Vectors
+  def __mul__(self, V):
+
+    return Vector(self.y * V.z - self.z * V.y,
+          self.z * V.x - self.x * V.z,
+          self.x * V.y - self.y * V.x)
+
+  # Method to define the representation of the Vector
+  def __repr__(self):
+
+    out = str(self.x) + "i "
+
+    if self.y >= 0:
+      out += "+ "
+    out += str(self.y) + "j "
+    if self.z >= 0:
+      out += "+ "
+    out += str(self.z) + "k"
+
+    return out
