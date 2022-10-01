4e4a40027b6a583d328080e1474f245e4bf07eca
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-01 20:38:41 +0100

    stuff

--- backends/test/questionmulti.py
+++ backends/test/questionmulti.py
@@ -2,4 +2,4 @@ import requests
 
 URL = "/api/quiz/add/multiple"
 
-data = {"question":"question", "quiz_id":1, "awnsers":[{"awnser":"test awnser 1", "correct":False}]}
+data = {"question":"question", "quiz_id":1, "awnsers":[{"awnser":"test awnser 1", "correct":False}, {"awnser":"test awnser 2", "correct":False}, {"awnser":"never gonna give you up", "correct":True}]}
