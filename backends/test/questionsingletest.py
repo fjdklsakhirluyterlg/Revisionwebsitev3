098b1722877937cbed239b53868bb89f67efd639
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-01 20:14:33 +0100

    stuff

--- backends/test/questionsingletest.py
+++ backends/test/questionsingletest.py
@@ -2,4 +2,4 @@ import requests
 
 URL = "http://127.0.0.1:5090/api/quiz/add/single"
 
-data = {"question":"test question", "awnser":"test awnser", "type":"text", "error":""}
+data = {"question":"test question", "awnser":"test awnser", "type":"text", "error":"0"}
