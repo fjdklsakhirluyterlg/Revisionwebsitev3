478e01e9ba749ac7b5c430c0ebb21d73288b8f07
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-01 20:25:55 +0100

    stuff

--- backends/test/questionsingletest.py
+++ backends/test/questionsingletest.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "http://127.0.0.1:5090/api/quiz/add/single"
 
-data = {"question":"test question", "awnser":"test awnser", "type":"text", "error":0, "quiz_id":2}
+data = {"question":"test question", "awnser":"test awnser", "type":"text", "error":0, "quiz_id":1}
 
 res = requests.post(URL, json=data)
 print(res.json())
