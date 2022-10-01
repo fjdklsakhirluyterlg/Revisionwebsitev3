75fccc2a924b17a9be6c03696481c6448e6540a5
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-01 20:02:51 +0100

    stuff

--- backends/quiz.py
+++ backends/quiz.py
@@ -171,6 +171,5 @@ def add_quiz_multiple_awnser_things():
         correct = a["correct"]
         new_awnser = Multiawnser(awnser=text, correct=correct, question_id=id)
         db.session.add(new_awnser)
-        db.session.commit()
     
 # app.register_blueprint(quiz, url_prefix="/")
