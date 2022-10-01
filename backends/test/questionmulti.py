eab1dd0a1c93e62d1c471a3f7934fad6307948ff
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-01 20:39:18 +0100

    stuff

--- backends/test/questionmulti.py
+++ backends/test/questionmulti.py
@@ -3,3 +3,6 @@ import requests
 URL = "/api/quiz/add/multiple"
 
 data = {"question":"question", "quiz_id":1, "awnsers":[{"awnser":"test awnser 1", "correct":False}, {"awnser":"test awnser 2", "correct":False}, {"awnser":"never gonna give you up", "correct":True}]}
+
+res = requests.post(URL, json=data)
+print(res.json())
