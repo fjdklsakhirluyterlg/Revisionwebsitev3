656bceef7ba9d5a995b40fd53612bbe4a75c7ec0
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-02 12:21:30 +0100

    stuff

--- backends/static/quiz.js
+++ backends/static/quiz.js
@@ -15,4 +15,11 @@ function check_multi(id){
     const classx = document.getElementById(`mulit${id}`).className
     const question_id = classx.substring(4)
     var data = {"awnser_id":id, "question_id":question_id}
+    fetch("/api/quiz/check/single", {
+        method : "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    }).then(res => res.json()).then(obj => console.log(obj.message)).catch((error) => {
+        console.error('Error:', error);
+    })
 }
