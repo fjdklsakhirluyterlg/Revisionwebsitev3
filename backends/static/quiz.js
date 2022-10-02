b7e933cdfb56678008617ceb584fa978d8fec05b
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-02 11:47:32 +0100

    stuff

--- backends/static/quiz.js
+++ backends/static/quiz.js
@@ -1,4 +1,9 @@
 function check_single(id){
     const awnser = document.getElementById(`inputsingle${id}`).value
-    var data = {}
+    var data = {"id":id, "awnser":awnser}
+    fetch("/api/quiz/check/single", {
+        method : "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    })
 }
