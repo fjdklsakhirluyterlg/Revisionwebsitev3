c10048450e06d2648e24145663d2c89fed078636
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-03 21:02:01 +0100

    stuff

--- backends/upload.py
+++ backends/upload.py
@@ -62,5 +62,29 @@ def al_that_user_uploaded():
             files.append(f)
     return render_template("files.html", files=files)
 
+@login_required
+@upload.route("/upload/banner", methods=["GET", "POST"])
+def upload_files_to_server():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        names = []
+        for file in request.files.getlist('file'):
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                names.append(filename)
+                curdir = os.getcwd()
+                namex = f"{current_user.name}.{current_user.id}.{filename}"
+                name = os.path.join(f"{curdir}/backends/banners/", namex)
+                file.save(name)
+        
+        return "uploaded"
+    else:
+        return render_template("upload.html")
+
 
 # app.register_blueprint(upload, url_prefix="/")
