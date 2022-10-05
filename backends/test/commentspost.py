bcdc0b70f24676cc03874295f3d8cfa2e9bd216c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-05 19:06:42 +0100

    stuff

--- backends/test/commentspost.py
+++ backends/test/commentspost.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "http://127.0.0.1:5090/api/community/add/comment/post"
 
-data = {"text":"test reply", "post_id":3, "author": "armaan", "parent_id":11}
+data = {"text":"test reply nested", "post_id":3, "author": "armaan", "parent_id":12}
 
 res = requests.post(URL, json=data)
 print(res.json())
