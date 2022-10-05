5a1f176ad20a01781e39fa8705022c947c2655ea
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-05 18:54:12 +0100

    stuff

--- backends/test/commentspost.py
+++ backends/test/commentspost.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "http://127.0.0.1:5090/api/community/add/comment/post"
 
-data = {"text":"test comment", "post_id":3, "author": "armaan", "parent_id":None}
+data = {"text":"test reply", "post_id":3, "author": "armaan", "parent_id":11}
 
 res = requests.post(URL, json=data)
 print(res.json())
