e1fec0307f8e069bb24e51cb695ae07a5121d7e0
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-08 21:19:03 +0100

    stuff

--- backends/notes.py
+++ backends/notes.py
@@ -1,1 +1,4 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
+
+note = Blueprint("note", __name__)
+
