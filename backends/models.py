77752e68e0fe322b60b8885d20780fa3f8f33132
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-08 21:24:10 +0100

    stuff

--- backends/models.py
+++ backends/models.py
@@ -348,4 +348,5 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     blog_id = db.Column(db.Integer, db.ForeignKey("blog.id"), nullable=True, default=None)
     post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=True, default=None)
+    stack_id = db.Column(db.Integer, db.ForeignKey("stack.id"), nullable=True, default=None)
 
