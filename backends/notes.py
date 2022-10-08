4346ac88238383b53f70f469c242f961aa419e68
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-08 21:38:42 +0100

    stuff

--- backends/notes.py
+++ backends/notes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
-
+from .import db
 from .models import Note
 
 note = Blueprint("note", __name__)
@@ -8,3 +8,9 @@ note = Blueprint("note", __name__)
 def api_add_a_note_thing():
     data = request.get_json()
     text = data["text"]
+    user_id = data["user_id"]
+
+    new = Note(text=text, user_id=user_id)
+    db.session.add(new)
+    db.session.commit()
+
