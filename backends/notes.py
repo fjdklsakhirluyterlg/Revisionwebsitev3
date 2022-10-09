57d8a746b4662f96a10e0b177401139d0684577e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-09 16:55:13 +0100

    stuff

--- backends/notes.py
+++ backends/notes.py
@@ -46,7 +46,7 @@ def view_note_thing(id):
 
 @notes.route("/api/notes/view/all")
 def view_all_notes():
-    notes = Note.query.order_by(Note.id)
+    notes = Note.query.order_by(Note.likes)[::-1]
     length = 0
     dict = {"length":length}
     for note in notes:
