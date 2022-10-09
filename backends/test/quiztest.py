6e9232f4d8f53a3b1652d5054a4e92cd9e697f11
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-09 10:44:47 +0100

    stuff

--- backends/test/quiztest.py
+++ backends/test/quiztest.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "http://127.0.0.1:5090/api/quiz/add"
 
-data = {"user_id":1, "description":"test", "name":"testname", "category":["test"]}
+data = {"user_id":1, "description":"test", "name":"test", "category":["test"]}
 
 res = requests.post(URL, json=data)
 print(res.json())
