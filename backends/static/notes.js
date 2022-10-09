0d63d659a79cbc5aa070089124cebb6c1a8edae7
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-09 12:26:01 +0100

    stuff

--- backends/static/notes.js
+++ backends/static/notes.js
@@ -3,4 +3,11 @@ function submit(id){
 
     data = {"text":text, "user_id":id}
 
+    fetch("/api/notes/add", {
+        method : "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    }).then(res => res.json()
+    )
+
 }
