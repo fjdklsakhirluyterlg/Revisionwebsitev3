7a53fb6ee4340f33a7b698798876e4f564b69eab
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-09 16:57:32 +0100

    stuff

--- backends/notes.py
+++ backends/notes.py
@@ -47,10 +47,11 @@ def view_note_thing(id):
 @notes.route("/api/notes/view/all")
 def view_all_notes():
     notes = db.session.query(Note).all()
-    length = 0
+    length = len(notes)
     dict = {"length":length}
     for note in notes:
         dict[note.id] = note.text
-        length += 1
+
+    dict = {"hi":"bye"}
 
     return dict
