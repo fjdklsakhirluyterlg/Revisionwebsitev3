012475db20a2c0c9928c405803ebefaabcc48319
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-11 19:59:10 +0100

    stuff

--- backends/mailer.py
+++ backends/mailer.py
@@ -5,3 +5,5 @@ from email.message import EmailMessage
 def validate_user_with_email_better(address, securitykey, name):
     sender_email = "banerjee.armaan@hotmail.com"
     password = "Transport11."
+    subject = "HI there"
+    body = f"Hello there, you have been emailed from me, your email adress is {address}"
