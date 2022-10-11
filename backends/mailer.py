a03642443f9003175930d8ff0a114bd47de0f8e5
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-11 19:58:37 +0100

    stuff

--- backends/mailer.py
+++ backends/mailer.py
@@ -3,4 +3,5 @@ import smtplib
 from email.message import EmailMessage
 
 def validate_user_with_email_better(address, securitykey, name):
+    sender_email = "banerjee.armaan@hotmail.com"
     password = "Transport11."
