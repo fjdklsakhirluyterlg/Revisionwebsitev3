a3309ce666e9cb02f73aae58bdac35ec92caf699
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-13 21:42:06 +0100

    stuff

--- backends/frontend/src/api.svelte
+++ backends/frontend/src/api.svelte
@@ -3,7 +3,7 @@
     
 	onMount(async () => {
         const res = fetch("http://127.0.0.1:5090/api/notes/view/all")
-        data = (await res).json()
+        data = await res.json()
     })
 </script>
 
