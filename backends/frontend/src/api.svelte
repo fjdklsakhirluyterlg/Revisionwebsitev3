22f12379feacf179b35363d35cc082e790341080
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-13 21:36:39 +0100

    stuff

--- backends/frontend/src/api.svelte
+++ backends/frontend/src/api.svelte
@@ -1,7 +1,5 @@
 <script>
-    import { loop_guard } from "svelte/internal";
-
-
+    
 	const getRandomUser = async () => {
 		var response = await fetch('http://127.0.0.1:5090/api/notes/view/all');
 		var result = await response.json();
