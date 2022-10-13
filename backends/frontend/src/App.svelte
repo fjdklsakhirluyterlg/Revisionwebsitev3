3c59e76f77182537851050a6436a8ea6a68b818d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-13 22:00:14 +0100

    stuff

--- backends/frontend/src/App.svelte
+++ backends/frontend/src/App.svelte
@@ -1,8 +1,10 @@
 <script>
     import Api from "./Api.svelte";
+    import Test from "./Test.svelte"
 </script>
 
 <main>
 	<p>never gonna give you up </p>
     <Api></Api>
+    <Test></Test>
 </main>
