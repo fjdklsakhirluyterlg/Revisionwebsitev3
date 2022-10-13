db3babd10554dd0f7b7804da6544fd735a99a8b2
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-13 20:03:45 +0100

    stuff

--- backends/frontend/src/api.svelte
+++ backends/frontend/src/api.svelte
@@ -1,6 +1,6 @@
 <script>
 	const getRandomUser = async () => {
-		var response = await fetch('https://randomuser.me/api/');
+		var response = await fetch('http://127.0.0.1:5090/api/notes/view/all');
 		var result = await response.json();
 		return result;
 	}
@@ -12,8 +12,8 @@
 
 {#await userPromise}
 	<h2>Loading....</h2>
-{:then users}
-	<h2>{users.results[0].name.first}</h2>
+{:then notes}
+	<p>notes.1</p>
 {:catch err}
 	<h2>Error while loading the data</h2>
 {/await}
