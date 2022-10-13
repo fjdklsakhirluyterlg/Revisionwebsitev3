770b8d10620417e6773deb1fb18db880e0d6f0a4
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-13 21:45:51 +0100

    stuff

--- backends/frontend/src/api.svelte
+++ backends/frontend/src/api.svelte
@@ -1,11 +1,13 @@
 <script>
     import { onMount } from 'svelte';
+
+    data = {}
     
 	onMount(async () => {
         const res = fetch("http://127.0.0.1:5090/api/notes/view/all")
         data = await res.json()
     })
 
-    for (var i = 0; i < Object.keys(data).length; i++)
+    
 </script>
 
