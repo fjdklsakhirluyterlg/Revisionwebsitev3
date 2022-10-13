aa9744ab91f0bc91348ce30d1884223d645c4065
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-13 21:41:19 +0100

    stuff

--- backends/frontend/src/api.svelte
+++ backends/frontend/src/api.svelte
@@ -1,25 +1,8 @@
 <script>
     import { onMount } from 'svelte';
     
-	const getRandomUser = async () => {
-		var response = await fetch('http://127.0.0.1:5090/api/notes/view/all');
-		var result = await response.json();
-		return result;
-	}
-	
-	let userPromise = getRandomUser();
-
-    function consolelogit(stuff){
-        console.log(stuff)
-    }
+	onMount(async () => {
+        const res = fetch("http://127.0.0.1:5090/api/notes/view/all")
+    })
 </script>
 
-{#await userPromise}
-	<h2>Loading....</h2>
-{:then notes}
-    {#each {length: 6} as _, i}
-    {notes[i]}
-    {/each}
-{:catch err}
-	<h2>Error while loading the data</h2>
-{/await}
