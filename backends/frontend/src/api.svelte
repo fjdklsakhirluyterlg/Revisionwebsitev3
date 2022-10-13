38a90cc569cd3220259db3d540e737eb4a927c0d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-13 20:23:57 +0100

    stuff

--- backends/frontend/src/api.svelte
+++ backends/frontend/src/api.svelte
@@ -19,7 +19,7 @@
 	<h2>Loading....</h2>
 {:then notes}
     {#each {length: 6} as _, i}
-    <html lang="en">{notes[i]}</html>
+    {notes[i]}
     {/each}
 {:catch err}
 	<h2>Error while loading the data</h2>
