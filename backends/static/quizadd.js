66a77267fe82219f1f967e4501bad384cf9982d9
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-15 19:30:53 +0100

    stuff

--- backends/static/quizadd.js
+++ backends/static/quizadd.js
@@ -2,7 +2,7 @@ function sumbit_quiz(id){
     var name = document.getElementById("name")
     var description = document.getElementById("description")
     var user_id = id
-    var categories = document.getElementById("categories")
+    var categories = document.getElementById("categories").split(" ")
     fetch("/api/quiz/add")
 
 }
