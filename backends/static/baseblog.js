4861da8fa44094c8ba75de8f442e1b87b9a7b44a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-17 22:28:26 +0100

    stuff

--- backends/static/baseblog.js
+++ backends/static/baseblog.js
@@ -15,7 +15,7 @@ function change_state(id){
 
 function add_bookmark(user_id, blog_id){
     var note = document.getElementById("note").value
-    var data = {"user_id":user_id, "blog_id":blog_id, "note":""}
+    var data = {"user_id":user_id, "blog_id":blog_id, "note":note}
 
     fetch("/api/blog/bookmark/add", {
         method : "POST",
