cfc19c6eb3d62bc72aefb9a23606b08effbcc73b
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-18 11:08:33 +0100

    stuff

--- backends/static/viewuser.js
+++ backends/static/viewuser.js
@@ -1,4 +1,9 @@
 function follow_user(current, follow){
     data = {"current_user":current, "followed":follow}
 
+    fetch("follow/user", {
+        method : "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    })
 }
