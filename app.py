488036a51eab053c3b3cf523155ff41aaccf3b68
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-18 21:01:30 +0100

    stuff

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from backends import create_app
 import os
-import ssl
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('certificate.crt', 'private.key')
-
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5090, host="0.0.0.0", ssl_context=context)
+    app.run(debug=True, port=5090, host="0.0.0.0")
