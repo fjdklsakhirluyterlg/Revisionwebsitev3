4937e4dfe0e21ad9505e6cb8082c0f328248ba7d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-18 17:17:43 +0100

    stuff

--- backends/user.py
+++ backends/user.py
@@ -265,7 +265,7 @@ def follow_user():
     followed = data["followed"]
     user = User.query.filter_by(id=followed).first()
     current = User.query.filter_by(id=current_user).first()
-    user.follow(current)
+    user.followed.append(current)
 
     return {"msg":"following"}
 
