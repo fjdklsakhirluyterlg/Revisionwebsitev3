a60ae4e244cf5f7f3c8ccf7b5f1868558990fd68
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-18 11:05:31 +0100

    stuff

--- backends/user.py
+++ backends/user.py
@@ -108,7 +108,8 @@ def view_user(id):
     awnsers = user.awnsers
     
     if current_user.is_authenticated and id != current_user.id:
-        return render_template("viewuser.html", name=name, comments=zip(comments, l), posts=posts, awnsers=awnsers)
+        return render_template("viewuser.html", name=name, comments=zip(comments, l), posts=posts, awnsers=awnsers, show=True)
+    return render_template("viewuser.html", name=name, comments=zip(comments, l), posts=posts, awnsers=awnsers, show=False)
 
 @login_required
 @user.route("/user/validate/<securitykey>")
