e2183cb318e27f241c084666fb502ff9ec4ce6f3
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-19 19:22:33 +0100

    stuff

--- backends/user.py
+++ backends/user.py
@@ -50,6 +50,8 @@ def dashboard():
         followed = usern.followed
         followednames = [folln.name for folln in followed]
 
+        following = usern.following
+
         return render_template('dashboard.html', name=name, email=email, joined=joined, id=id, comments=zip(comments, l), points=points, ask=ask, notifications=notifications, bookmarks=zip(bookmarks, blog_bookmarks), posts=posts, awnsers=awnsers, notes=notes, followers=follwernames, followed=followednames)  
     except:
         return redirect("/login")
