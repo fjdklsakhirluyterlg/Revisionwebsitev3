bbdf7e00cbaa788c8f0e4409e02b5e6fc27b48d6
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-19 10:13:57 +0100

    stuff

--- backends/go_stuff/test.go
+++ backends/go_stuff/test.go
@@ -4,12 +4,12 @@ import(
     "fmt"                           
     "math"                          
 )
-func square(inte float64){
+func squarert(inte float64){
     fmt.Println(math.Sqrt(inte))
 }   
 
 func main() {
     fmt.Println("Hello, World!")
     var x float64 = 32.0
-    square(x)
+    squarert(x)
 }
