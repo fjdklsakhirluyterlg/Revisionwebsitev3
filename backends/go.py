3d4648ab482fb64c97be59851520ba22d727363c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-19 21:16:48 +0100

    stuff

--- backends/go.py
+++ backends/go.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 import ctypes
 
 library = ctypes.cdll.LoadLibrary('./go_stuff/test.so')
