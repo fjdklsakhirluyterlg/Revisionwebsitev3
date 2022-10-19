0bc6548361d376548800a49e7bc8307935712b31
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-19 10:34:34 +0100

    stuff

--- backends/go.py
+++ backends/go.py
@@ -3,3 +3,7 @@ import ctypes
 library = ctypes.cdll.LoadLibrary('./go_stuff/test.so')
 hello_world = library.helloworld
 hello_world()
+
+hello = library.hello
+hello.argtypes = [ctypes.c_char_p]
+hello("everyone".encode('utf-8'))
