8a4a99f7b260e814b9cc8d0f1a3eca3b12642fec
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-20 17:40:52 +0100

    stuff

--- backends/blogs.py
+++ backends/blogs.py
@@ -586,7 +586,7 @@ def delete_bookmark(id):
 
 @blogs.route("/blog/bookmark/edit/<id>")
 def edit_bookmark(id):
-    pass
+    bookmarkd = Bookmark.query.filter_by(id=id).first()
     
 @blogs.route("/blogs/find/related/<id>")
 def related_tags_thingy(id):
