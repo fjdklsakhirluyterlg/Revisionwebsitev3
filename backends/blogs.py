985725a53133d59000ca3aa42a3b5c461e17aa43
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-20 17:49:32 +0100

    stuff

--- backends/blogs.py
+++ backends/blogs.py
@@ -590,7 +590,7 @@ def edit_bookmark(id):
     note = request.form.get("note")
     bookmark.note = note
     db.session.commit()
-    return "edited"
+    return redirect(f"/blog/view/{id}")
     
 @blogs.route("/blogs/find/related/<id>")
 def related_tags_thingy(id):
