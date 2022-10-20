ece4a5ed63ec31bf7fc42eb042a837b8a6919921
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-20 10:23:19 +0100

    stuff

--- backends/blogs.py
+++ backends/blogs.py
@@ -564,7 +564,7 @@ def addbookmark_to_blog(id):
     if request.method == "POST":
         blog = db.session.query(Blog).filter(Blog.id == id).first()
         note = request.form.get("note")
-        cid = request.form.get("user_id")
+        cid = request.args.get("user_id")
         new = Bookmark(blog_id=blog.id, user_id=cid, note=note)
         db.session.add(new)
         db.session.commit()
