d4d57c078234e4b5a1c18c4e1f2cc211e2f26d84
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-20 10:30:43 +0100

    stuff

--- backends/user.py
+++ backends/user.py
@@ -30,11 +30,11 @@ def dashboard():
         
         notifications = db.session.query(Notifications).filter(Notifications.user_id == id).all()[::-1]
         bookmarks = usern.bookmarks
-        # blog_bookmarks = []
-        # for bookmark in bookmarks:
-        #     bookmark_blog_id = bookmark.blog_id
-        #     hog = Blog.query.filter_by(id=bookmark_blog_id).first()
-        #     blog_bookmarks.userend(hog.title)
+        blog_bookmarks = []
+        for bookmark in bookmarks:
+            bookmark_blog_id = bookmark.blog_id
+            hog = Blog.query.filter_by(id=bookmark_blog_id).first()
+            blog_bookmarks.append(hog.name)
             
         posts = usern.posts
         awnsers = usern.awnsers
