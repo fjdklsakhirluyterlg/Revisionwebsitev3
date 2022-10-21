f66c287f441b16ebbbe0381257f399b9fb990e36
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-21 11:53:45 +0100

    stuff

--- backends/chat.py
+++ backends/chat.py
@@ -62,8 +62,8 @@ def chat_with_other_users(id):
 
 @socketio.on("message")
 def messageReceived(message):
-    print(f'message : {message["message"]}')
-    socketio.send(message["message"], room=message["message"])
+    print(f'message : {message}')
+    socketio.send(message["message"], room=message["id"])
     
 
 @socketio.on("left")
