25a39fe4d41594f36914a03717a5e9e6c930bd9a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-23 11:36:01 +0100

    stuff

---
+++ backends/frontend/src/Blogs.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { onMount } from 'svelte';
+
+    var data = {};
+
+    onMount(async () => {
+        const res = await fetch("http://127.0.0.1:5090/blogs")
+        data = await res.json()
+    })
+
+    
+</script>
