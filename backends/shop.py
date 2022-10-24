d23cc1b3187450c2df56deebd70287194b94ea93
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-24 21:56:38 +0100

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 from . import db
 from flask_login import current_user, login_required
-from .models import Item, Object
+from .models import Item, Object, Checkout
 
 shop = Blueprint("shop", __name__)
 
@@ -33,5 +33,6 @@ def view_all_items():
 def buy_item_thing():
     data = request.get_json()
     user_id = data["user_id"]
+    new = Checkout(user_id=user_id)
 
     
