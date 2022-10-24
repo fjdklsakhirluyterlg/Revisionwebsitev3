0e517b09af07c1ba784a20715caeb5334246df35
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-24 21:36:26 +0100

    stuff

--- backends/test/shopadd.py
+++ backends/test/shopadd.py
@@ -2,4 +2,4 @@ import requests
 
 URL = "http://127.0.0.1:5090/api/shop/item/add"
 
-data = {"title":"test", "description":"this is a description", "price":"10"}
+data = {"title":"test", "description":"this is a description", "price":"10", "stock":10}
