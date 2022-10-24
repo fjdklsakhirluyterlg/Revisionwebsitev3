56a4fb392cab32bb04ee008632c7bd8b0c8671f0
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-24 21:41:50 +0100

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -24,4 +24,5 @@ def view_all_items():
     items = Item.query.all()
     dictx = {}
     for item in items:
-        dict[item.id] = {"title":item.title, "price":item.price, "description":item.description, "stock":item.stock}
+        dictx[item.id] = {"title":item.title, "price":item.price, "description":item.description, "stock":item.stock}
+    
