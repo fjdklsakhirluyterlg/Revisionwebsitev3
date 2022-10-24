42f8c7770d67ef1d3b8d1eb232bc5d83f26543fe
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-24 21:38:18 +0100

    stuff

--- backends/test/shopadd.py
+++ backends/test/shopadd.py
@@ -4,3 +4,5 @@ URL = "http://127.0.0.1:5090/api/shop/item/add"
 
 data = {"title":"test", "description":"this is a description", "price":"10", "stock":3}
 
+res = requests.post(URL, json=data)
+print(res.json())
