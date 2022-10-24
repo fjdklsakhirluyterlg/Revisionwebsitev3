2987099d977aeada932130d9a491774fd5cacb30
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-24 22:25:10 +0100

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -1,4 +1,3 @@
-from urllib.parse import parse_qs
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 from . import db
 from flask_login import current_user, login_required
