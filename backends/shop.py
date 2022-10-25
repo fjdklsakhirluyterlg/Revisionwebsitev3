29d6a72a1f3ae52a3b8fe8781ae2421eb0a5f0ac
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-25 20:01:31 +0100

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -56,6 +56,7 @@ def bu_the_checkout():
     checkout = Checkout.query.filter_by(id=id).first()
     for object in checkout.objects:
         total_price += object.price
+        object.sold = True
 
 
     
