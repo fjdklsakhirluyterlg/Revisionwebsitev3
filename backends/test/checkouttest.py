18413e540beb71877de88224cf2cbd815c71d38f
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-25 22:37:46 +0100

    stuff

--- backends/test/checkouttest.py
+++ backends/test/checkouttest.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "http://127.0.0.1:5090/api/shop/checkout/add"
 
-data = {"user_id":1, "objects":[1, 2]}
+data = {"user_id":1, "objects":["1", "2"]}
 
 res = requests.post(URL, json=data)
 print(res.json())
