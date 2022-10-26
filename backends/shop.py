f35c693ac434cceebfbdf14ddff902b2629f515c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-26 21:11:38 +0100

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -71,6 +71,8 @@ def view_add_for_shopadd():
                 file.save(name)
 
         return "added"
+    else:
+        return render_template("itemadd.html")
 
 @shop.route("/api/shop/account/create")
 def create_shop_acconut():
