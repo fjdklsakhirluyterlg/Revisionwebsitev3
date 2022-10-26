df2ef240f1d1ddae7bb6ef5d794f765f10234e96
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-26 20:37:20 +0100

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 from . import db
 from flask_login import current_user, login_required
-from .models import Item, Object, Checkout
+from .models import Item, Object, Checkout, User
 
 shop = Blueprint("shop", __name__)
 
