78eeebe122b36990cf5086de756f36cf8acc1d7b
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-28 12:13:06 +0100

    stuff

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -11,3 +11,5 @@ def test_home_page_post():
     flask_app = create_app()
     with flask_app.test_client() as test_client:
         response = test_client.post('/')
+        assert response.status_code == 405
+        assert b"hi there" not in response.data
