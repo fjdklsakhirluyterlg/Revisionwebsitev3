d4898efc4868d8cbacb10b7f62a0c8df4745d089
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-28 12:12:28 +0100

    stuff

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -1,8 +1,13 @@
 from backends import create_app
 
-def test_home_page():
+def test_home_page_get():
     flask_app = create_app()
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
         assert b"hi there" in response.data
+
+def test_home_page_post():
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/')
