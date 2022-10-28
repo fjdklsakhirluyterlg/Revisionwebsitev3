0ca20bd1b4f86e9e095ca5644a7865d2ba3220ae
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-28 10:23:33 +0100

    stuff

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -14,3 +14,5 @@ def test_user_model():
     name = "test"
     password = "password"
     security_key = mank_random_long_id(64)
+    email = "test@test.com"
+    user = User(name=name, email=email, password=generate_password_hash(password, method='sha256'), security_key=security_key)
