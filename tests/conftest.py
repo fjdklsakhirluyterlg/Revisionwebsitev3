0031744e0313e37505c0727bf1c049dccb966654
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-28 16:20:45 +0100

    stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from backends.models import User
 from werkzeug.security import generate_password_hash
 import random
+from backends import create_app
 
 def mank_random_long_id(length):
     characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -19,3 +20,10 @@ def new_user():
     email = "test@test.com"
     user = User(name=name, email=email, password=generate_password_hash(password, method='sha256'), security_key=security_key)
     return user
+
+@pytest.fixture(scope="module")
+def test_client():
+    flask_app = create_app('flask_test.cfg')
+    with flask_app.test_client() as testing_client:
+        with flask_app.app_context():
+            yield testing_client
