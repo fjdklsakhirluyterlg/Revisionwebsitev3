d0dce607342fb5a15046d50f1657a56b9bbe9b62
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-28 16:17:11 +0100

    stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,19 @@
 import pytest
 from backends.models import User
+from werkzeug.security import generate_password_hash
+import random
 
+def mank_random_long_id(length):
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    result = ""
+    charactersLength = len(characters) - 1
+    for i in range(length):
+        result += characters[random.randint(0, charactersLength)]
+    return result
+
+def new_user():
+    name = "test"
+    password = "password"
+    security_key = mank_random_long_id(64)
+    email = "test@test.com"
+    user = User(name=name, email=email, password=generate_password_hash(password, method='sha256'), security_key=security_key)
