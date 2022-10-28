bb2bc37cb3757161a2bc9c19c534fd1b04d18659
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-28 10:18:56 +0100

    stuff

---
+++ tests/unit/test_models.py
@@ -0,0 +1,13 @@
+from backends.models import User
+import random
+
+def mank_random_long_id(length):
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    result = ""
+    charactersLength = len(characters) - 1
+    for i in range(length):
+        result += characters[random.randint(0, charactersLength)]
+    return result
+
+def test_user_model():
+    pass
