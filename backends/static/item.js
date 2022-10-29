5089b753a0f67f329ab159234606a39dff9ec6c7
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-29 20:19:16 +0100

    stuff

--- backends/static/item.js
+++ backends/static/item.js
@@ -1,4 +1,4 @@
-function checkout(user_id, objects, amount){
+function checkout(user_id, objects){
     var act = objects.slice(0, amount)
     var data = {"user_id":user_id, "objects":act}
     fetch("/api/shop/checkout/add", {
