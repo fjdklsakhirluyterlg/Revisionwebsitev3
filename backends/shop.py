ea48e15c5a2c1a1be7b65fff9ac3d5f6dc37d22f
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-30 20:31:04 +0000

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -225,5 +225,5 @@ def search_shop():
 
 @shop.route("/api/test/multiple/list")
 def multiple_list_test():
-    items = request.args["q"]
+    items = request.args.get("q")
     return jsonify({"items":items})
