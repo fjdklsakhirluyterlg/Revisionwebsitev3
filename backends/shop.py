44ff9fafb9c98785b3226b6eabbf229b794beba0
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-04 21:46:16 +0000

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -247,7 +247,7 @@ def see_user_shop_account_name(name):
         for item in itmes:
             names.append(item.title)
         description = user.description
-        
+        dict = {}
 
 
 @shop.route("/api/test/multiple/list") 
