6e704fc179078059c56b7b73e434bc5bac8c7943
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-05 21:52:06 +0000

    stuff

--- backends/chat.py
+++ backends/chat.py
@@ -83,7 +83,7 @@ def joinRoom(message):
     join_room(room)
     print(f"joined room: {room}")
     data = {"message":"someone joined","name":"admin"}
-    socketio.emit("message", data, room=message["id"])
+    socketio.emit("joined", data, room=message["id"])
     
 @login_required
 @chat.route('/chat/add-text/<id>', methods=["POST"])
