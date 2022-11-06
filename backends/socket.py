3957688aeba43cc2798065de9a7b338a118217eb
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-06 08:40:30 +0000

    stuff

--- backends/socket.py
+++ backends/socket.py
@@ -1,4 +1,7 @@
 from . import socketio
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
+
+socket = Blueprint("socket", __name__)
 
 @socketio.on("note-change")
 def note_change(message):
