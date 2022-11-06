112df66c86b69005d95957a6ba2c920878795387
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-06 08:43:24 +0000

    stuff

--- backends/socket.py
+++ backends/socket.py
@@ -1,9 +1,11 @@
 from . import socketio
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
+from flask_socketio import join_room
 
 socket = Blueprint("socket", __name__)
 
 @socketio.on("note-change")
 def note_change(message):
     room = message["id"]
-    
+    join_room(room)
+    socketio.emit("note-changed", message, room=room)
