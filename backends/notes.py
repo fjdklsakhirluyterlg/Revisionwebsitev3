c2b63dab911cd93d4833c05b5ab16987dc2a409f
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-06 17:18:20 +0000

    stuff

--- backends/notes.py
+++ backends/notes.py
@@ -81,6 +81,6 @@ def user_on_notes(message):
 @socketio.on("note-change")
 def note_change(message):
     room = message["id"]
-    print(room)
+    # print(room)
     join_room(room)
     socketio.emit("note-changed", message, room=room)
