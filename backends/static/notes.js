da42c4698e737902289c1b8a8ee83b568d7d2f25
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-06 17:34:27 +0000

    stuff

--- backends/static/notes.js
+++ backends/static/notes.js
@@ -72,7 +72,7 @@ function edit() {
 
 window.onload() = function(){
     var editor = document.getElementById("editor")
-    editor.addEventListener("change", edit())
+    editor.addEventListener("change", edit)
 }
 
 // window.onload = function(){
