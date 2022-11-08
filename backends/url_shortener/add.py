346b47850b4d8a4a50e4b9261b15a35a6daf15ba
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-08 22:37:00 +0000

    stuff

--- backends/url_shortener/add.py
+++ backends/url_shortener/add.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
 from backends.models import urlshortner
+from backends import db
 
 add_url = Blueprint("add_url", __name__)
 
