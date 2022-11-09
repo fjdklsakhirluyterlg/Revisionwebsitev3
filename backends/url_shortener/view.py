92c3425f8d9cc595a23c3ddf4250d211537006ed
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-09 20:09:05 +0000

    stuff

--- backends/url_shortener/view.py
+++ backends/url_shortener/view.py
@@ -1,1 +1,3 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
+
+view_url = Blueprint("view_url", __name__)
