368f95815e325438ae5e1b0c87556b28b2fbf764
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-09 20:02:00 +0000

    stuff

--- backends/url_shortener/add.py
+++ backends/url_shortener/add.py
@@ -26,7 +26,7 @@ def generate_random_id():
     return result
 
 
-@add_url.route('/api/urls/shortner/add')
+@add_url.route('/api/urls/shortner/add', emthods=["POST"])
 def add_url_to_shortenere():
     data = request.get_json()
     free = False
