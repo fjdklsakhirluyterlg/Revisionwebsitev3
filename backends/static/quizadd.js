700d918c008d44243c56c1a2e5c2713781978362
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-10 22:08:20 +0000

    stuff

--- backends/static/quizadd.js
+++ backends/static/quizadd.js
@@ -16,7 +16,7 @@ function sumbit_quiz(id){
 function add_multi(){
     var multi = documnet.getElementById("multi")
     var clone = multi.content.cloneNode(true);
-    
+    document.append(clone)
 
 }
 
