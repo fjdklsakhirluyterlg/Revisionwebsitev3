24fa29427d2d1dd77f558ce710b8cf6ce2e12f61
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-12 21:17:01 +0000

    stuff

--- backends/go_stuff/sockets/client/main.go
+++ backends/go_stuff/sockets/client/main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 )
 
-// Application constants, defining host, port, and protocol.
 const (
 	connHost = "localhost"
 	connPort = "8980"
@@ -16,7 +15,7 @@ const (
 )
 
 func main() {
-	// Start the client and connect to the server.
+
 	fmt.Println("Connecting to", connType, "server", connHost+":"+connPort)
 	conn, err := net.Dial(connType, connHost+":"+connPort)
 	if err != nil {
@@ -24,24 +23,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Create new reader from Stdin.
 	reader := bufio.NewReader(os.Stdin)
 
-	// run loop forever, until exit.
 	for {
-		// Prompting message.
+
 		fmt.Print("Text to send: ")
 
-		// Read in input until newline, Enter key.
 		input, _ := reader.ReadString('\n')
 
-		// Send to socket connection.
 		conn.Write([]byte(input))
 
-		// Listen for relay.
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 
-		// Print server relay.
 		log.Print("Server relay: " + message)
 	}
 }
