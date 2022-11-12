bddda5927fc12f626ba2a83fd8b6bc8e05ed4777
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-12 20:57:17 +0000

    stuff

--- backends/static/addurl.js
+++ backends/static/addurl.js
@@ -1,4 +1,9 @@
 function submit(user_id){
-
-    var data = {}
+    var actual = document.getElementById("url").innerHTML
+    var data = {"actual":actual, "user_id":user_id}
+    fetch("/api/urls/shortner/add", {
+        method : "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    })
 }
