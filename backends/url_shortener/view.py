8a3646e57963621c776cee91e4e17fc9e0f1d008
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-13 22:24:57 +0000

    stuff

--- backends/url_shortener/view.py
+++ backends/url_shortener/view.py
@@ -5,6 +5,7 @@ view_url = Blueprint("view_url", __name__)
 @view_url.route("/urls/<name>")
 def view_url_thing(name):
     url = Urlshortner.query.filter_by(id=name).first()
+    url.views += 1
     return f"""
     <head><script>window.location.href="{url}"</script></head>
     """
