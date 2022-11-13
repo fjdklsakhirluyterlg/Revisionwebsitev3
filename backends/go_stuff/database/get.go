dadfe6c52c306855a03d47200bb7d90eacccf3f2
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-13 18:17:21 +0000

    stuff

--- backends/go_stuff/database/get.go
+++ backends/go_stuff/database/get.go
@@ -3,6 +3,25 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"time"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+func main() {
+	db, err := sql.Open("sqlite3", "../../database.db")
+	checkErr(err)
+	rows, err := db.Query("SELECT * FROM User")
+	checkErr(err)
+    
+	var count int
+
+	for rows.Next() {
+		count += 1
+	}
+
+	fmt.Print(count)
+
+}
+
+func checkErr(err error) {
+	panic("unimplemented")
+}
