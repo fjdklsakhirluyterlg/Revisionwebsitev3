d855ab03b9dbcb3d16e5cc8e35cc87b3a1c9c4cc
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-15 21:36:40 +0000

    stuff

--- backends/shop.py
+++ backends/shop.py
@@ -165,6 +165,7 @@ def bu_the_checkout():
     
     out = "".join(str(e) for e in names)
     new_text = f"You have purchased {out}"
+    new_notifaction = Notifications(user_id=userd_id, text=new_text)
     db.session.add(new_text)
     db.session.commit()
 
