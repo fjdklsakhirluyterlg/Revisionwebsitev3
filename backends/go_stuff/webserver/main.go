79b001eeedf6f584bd2e7ed71e44cbc68f2047e3
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-16 21:38:46 +0000

    stuff

--- backends/go_stuff/webserver/main.go
+++ backends/go_stuff/webserver/main.go
@@ -6,3 +6,20 @@ import (
 	"log"
 )
 
+const (
+	Host = "localhost"
+	Port = "8090"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a Simple HTTP Web Server!")
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	err := http.ListenAndServe(Host+":"+Port, nil)
+	if err != nil {
+		log.Fatal("Error Starting the HTTP Server : ", err)
+		return
+	}
+}
