21c8af5f1737b3a62e82b001ef7d28a270b1db4a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-16 21:42:37 +0000

    stuff

---
+++ backends/supplementary/sattolo.py
@@ -0,0 +1,8 @@
+from random import randrange
+
+def sattolo_cycle(items) -> None:
+    i = len(items)
+    while i > 1:
+        i = i - 1
+        j = randrange(i)  # 0 <= j <= i-1
+        items[j], items[i] = items[i], items[j]
