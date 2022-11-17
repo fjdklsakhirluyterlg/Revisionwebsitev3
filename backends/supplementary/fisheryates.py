3d518c087ffab2ce7275703ad5abe952c2fb8beb
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-17 17:57:11 +0000

    stuff

--- backends/supplementary/fisheryates.py
+++ backends/supplementary/fisheryates.py
@@ -1,5 +1,6 @@
 from random import randint
-def randomize (arr, n):
+def randomize (arr):
+    n = len(arr)
     for i in range(n-1,0,-1):
         j = randint(0,i+1)
         arr[i],arr[j] = arr[j],arr[i]
