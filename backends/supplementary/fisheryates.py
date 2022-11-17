4c59fcd949db056ba205919188d35fa0b0d53415
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-17 18:01:55 +0000

    stuff

--- backends/supplementary/fisheryates.py
+++ backends/supplementary/fisheryates.py
@@ -1,11 +1,7 @@
 from random import randint
-def randomise (arr):
+def randomise(arr):
     n = len(arr)
     for i in range(n-1,0,-1):
         j = randint(0,i+1)
         arr[i],arr[j] = arr[j],arr[i]
     return arr
-
-array = [1, 2, 7, 4, 3, 9, 8, 6]
-randomise(array)
-print(array)
