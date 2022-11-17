34ab347cf77158f1720e4f2d120afc252d7a9400
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-17 17:49:05 +0000

    stuff

--- backends/supplementary/fisheryates.py
+++ backends/supplementary/fisheryates.py
@@ -1,11 +1,6 @@
 from random import randint
 def randomize (arr, n):
-    # Start from the last element and swap one by one. We don't
-    # need to run for the first element that's why i > 0
     for i in range(n-1,0,-1):
-        # Pick a random index from 0 to i
         j = randint(0,i+1)
- 
-        # Swap arr[i] with the element at random index
         arr[i],arr[j] = arr[j],arr[i]
     return arr
