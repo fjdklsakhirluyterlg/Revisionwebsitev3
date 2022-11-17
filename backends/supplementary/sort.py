ff8884d92d89fa3545343efb43903cabba0aacb1
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-17 17:45:36 +0000

    stuff

--- backends/supplementary/sort.py
+++ backends/supplementary/sort.py
@@ -41,3 +41,9 @@ def mergeSort(arr):
 def bogosort(array):
     pass
 
+def arraySortedOrNot(arr):
+    n = len(arr)
+    if n == 1 or n == 0:
+        return True
+
+    return arr[0] <= arr[1] and arraySortedOrNot(arr[1:])
