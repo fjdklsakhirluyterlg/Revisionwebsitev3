05a523b0d56e7c726e7825474d2781df81c97e49
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 09:11:08 +0000

    stuff

--- backends/news/getdata.py
+++ backends/news/getdata.py
@@ -18,6 +18,7 @@ class RssFetcher:
     def __init__(self, url):
         headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
         self.url = url
+        self.headers = headers
 
 def get_simple_flying():
     URL = "https://simpleflying.com/"
