6fa29b6005067e22f9ddf713b7914d2b3aa83eef
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 21:37:06 +0000

    stuff

--- backends/models.py
+++ backends/models.py
@@ -538,3 +538,6 @@ class Headline(db.Model):
 class Calendar(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
