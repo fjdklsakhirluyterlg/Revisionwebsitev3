8495d00b445a03ee9dbd4567f04243fcee89b71b
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 08:37:15 +0000

    stuff

--- backends/models.py
+++ backends/models.py
@@ -522,3 +522,4 @@ class Newssource(db.Model):
 class Headline(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
