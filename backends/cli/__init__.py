43e6c7a8354be410fb61195d62cd5f187aefc944
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 19:46:13 +0000

    stuff

--- backends/cli/__init__.py
+++ backends/cli/__init__.py
@@ -1,9 +1,9 @@
 import click
 from flask import Blueprint
 
-cli = Blueprint("cli", __name__, cli_group=None)
+clix = Blueprint("cli", __name__, cli_group=None)
 
-@bp.cli.command('create')
+@clix.cli.command('create')
 @click.argument('name')
 def create(name):
     print(name)
