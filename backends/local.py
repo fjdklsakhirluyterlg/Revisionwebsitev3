6810257233e8ad51a7d34d968f4a22a4afbf3c62
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 19:17:29 +0000

    stuff

--- backends/local.py
+++ backends/local.py
@@ -1,3 +1,14 @@
-from backends.stastics.data import write_users
+from backends.models import User
+import csv
 
-write_users()
+def write_users():
+    users = []
+    query = User.query.all()
+    for q in query:
+        data = [q.name, q.timestamp, q.points, q.email]
+        users.append(data)
+
+    with open('users.csv', 'w', newline='') as file:
+        fieldnames = ["name", "timestamp", "points", "email"]
+        writer = csv.writer(file)
+        writer.writerows(users)
