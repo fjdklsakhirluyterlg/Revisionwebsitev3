ac7abcb0bbbcda9310936e17e1fe3d07b5a9687d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 11:21:26 +0000

    stuff

--- backends/news/getdata.py
+++ backends/news/getdata.py
@@ -77,5 +77,11 @@ def get_the_guardian():
     return data[-10:]
 
 def get_reuteurs():
-    URL = ""
+    URL = "https://www.reuters.com/"
+    tag = "a"
+    pattern = "text__text__1FZLe text__dark-grey__3Ml43 text__medium__1kbOh text__heading_5_and_half__3YluN heading__base__2T28j heading_5_half media-story-card__heading__eqhp9"
+    url = UrlFetcher(URL, tag, pattern)
+    data = url.get_data()
+    return data
 
+print(get_reuteurs())
