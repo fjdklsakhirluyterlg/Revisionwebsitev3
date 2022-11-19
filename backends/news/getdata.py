8a34e5e2fedf4ad5763b1bc5d1717e91eebd6da0
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 08:44:49 +0000

    stuff

--- backends/news/getdata.py
+++ backends/news/getdata.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 class UrlFetcher:
-    def __init__(self, url, pattern):
+    def __init__(self, url, tag, pattern):
         self.url = url
         self.pattern = pattern
+        self.tag = tag
     
     def get_data(self):
         URL = self.url
