908192c24b9b8b983db37edd75058df1a4ae679c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 21:48:29 +0000

    stuff

--- meta/writeusers.py
+++ meta/writeusers.py
@@ -3,9 +3,11 @@ from sql import get_users
 
 def write_users():
     users = []
-    users = get_users()[0]
+    users = get_users()
 
     with open('users.csv', 'w', newline='') as file:
         fieldnames = ["name", "timestamp", "points", "email"]
         writer = csv.writer(file)
         writer.writerows(users)
+
+write_users()
