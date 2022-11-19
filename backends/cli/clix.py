d29faee26c87a4496bccc59594dc2cafd8c55f6c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-19 21:30:05 +0000

    stuff

--- backends/cli/clix.py
+++ backends/cli/clix.py
@@ -2,13 +2,14 @@ import click
 from flask import Blueprint
 from backends import db
 from backends.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 clix = Blueprint("cli", __name__, cli_group=None)
 
 @clix.cli.command('create')
 @click.argument('name')
 def create(name):
-    print(name)
+    email = "admin@admin.com"
 
 @clix.cli.command("get")
 @click.argument("users")
