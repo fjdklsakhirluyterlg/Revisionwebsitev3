4b83ffd019482e5015217c50d2f6012d9feef49a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-20 16:45:47 +0000

    stuff

--- backends/hacking/write.py
+++ backends/hacking/write.py
@@ -5,7 +5,7 @@ connection = sqlite3.connect("./hack.db")
 crsr = connection.cursor()
 
 def check_if_databse_has_data():
-    sql_query = """USE hack SHOW tables"""
+    sql_query = """SELECT COUNT(DISTINCT `table_name`) FROM `information_schema`.`columns` WHERE `table_schema` = 'hack'"""
 
     crsr.execute(sql_query)
     ans = crsr.fetchall()
