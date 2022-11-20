011b7ac2c6c7a94f1aea33f431e9fc64e24e2e55
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-20 16:40:33 +0000

    stuff

--- backends/hacking/write.py
+++ backends/hacking/write.py
@@ -1,5 +1,8 @@
 import sqlite3
 
-connection = sqlite3.connect("")
+connection = sqlite3.connect("./hack.db")
 
 crsr = connection.cursor()
+
+def check_if_databse_has_data():
+    sql_query = """SELECT COUNT(DISTINCT `TABLE_NAME`) AS anyAliasName FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `table_schema` = 'hack'"""
