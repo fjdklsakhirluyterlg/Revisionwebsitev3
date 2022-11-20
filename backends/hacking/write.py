6f03aed66f8b38dbdf25a71146ebd80bf10e02e6
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-20 16:46:19 +0000

    stuff

--- backends/hacking/write.py
+++ backends/hacking/write.py
@@ -6,9 +6,11 @@ crsr = connection.cursor()
 
 def check_if_databse_has_data():
     sql_query = """SELECT COUNT(DISTINCT `table_name`) FROM `information_schema`.`columns` WHERE `table_schema` = 'hack'"""
-
-    crsr.execute(sql_query)
-    ans = crsr.fetchall()
-    return ans
+    try:
+        crsr.execute(sql_query)
+        ans = crsr.fetchall()
+        return ans
+    except:
+        return 0
 
 print(check_if_databse_has_data())
