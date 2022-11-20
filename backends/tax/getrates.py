b116a78b9502ce1f2ef4f74b67e60022b05fc0b2
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-20 19:51:31 +0000

    stuff

--- backends/tax/getrates.py
+++ backends/tax/getrates.py
@@ -2,3 +2,8 @@ class tax:
     def __init__(self, country, amount):
         self.country = country
         self.amount = amount
+    
+    def calculate_annual_from_monthy(self, amount):
+        return amount*12
+
+    
