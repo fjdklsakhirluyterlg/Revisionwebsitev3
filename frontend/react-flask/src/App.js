e8e896db739cbcbbb0523b58b333de7c4d39364d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-21 22:04:24 +0000

    stuff

--- frontend/react-flask/src/App.js
+++ frontend/react-flask/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <p>My name is joe</p>
     </div>
+    </Router>
   );
 }
 
