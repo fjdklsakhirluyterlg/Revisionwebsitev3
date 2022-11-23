a25a90800e282599e05e8b1f3de6d1ad40d3986a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-23 20:35:38 +0000

    stuff

--- backends/scammer/addscamer.py
+++ backends/scammer/addscamer.py
@@ -4,5 +4,5 @@ from flask import Blueprint, request, render_template
 scammer = Blueprint("scammer", __name__)
 
 @scammer.route("/scams/support/add")
-def add_scam_phone():
+def add_scam():
     pass
