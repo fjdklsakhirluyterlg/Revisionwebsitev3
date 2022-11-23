6af2b3e9cafb4b4fe215463306b17565d24cf26d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-23 21:46:58 +0000

    stuff

--- backends/stastics/readlogger.py
+++ backends/stastics/readlogger.py
@@ -3,6 +3,11 @@ def read_logger():
     file = "../../record.log"
     with open(file, "rb+") as f:
         f = f.readlines()
+    
+    list = []
+    for line in f:
+        if "(process_request_thread)" in f:
+            list.append(line)
     f.close()
 
 
