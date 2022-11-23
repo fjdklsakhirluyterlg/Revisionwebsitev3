b6a90428167ebb197dab8c29da9378886ed9e093
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-23 21:25:34 +0000

    stuff

--- backends/lib/httplib.py
+++ backends/lib/httplib.py
@@ -14,6 +14,6 @@ async def get(url, method):
 
 async def main_run():
     status, reason = asyncio.gather(get("https://www.python.org/", "GET"))
-    print(status)
+    print(status, reason)
 
 asyncio.run(main_run())
