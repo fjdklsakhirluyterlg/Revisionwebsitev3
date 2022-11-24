8c2633e174da1681897e8d5c85401b26ebe3ea98
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-24 20:02:23 +0000

    stuff

--- backends/downloads/youtubedownload.py
+++ backends/downloads/youtubedownload.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, send_file, flash, jsonify, Blueprint, Response, abort
+from pytube import YouTube
+
 
 youtube_downloader = Blueprint("youtube_downloader")
 
 @youtube_downloader.route("/api/downloads/youtube", methods="POST")
 def download_youtube_video():
-    pass
+    data = request.get_json()
