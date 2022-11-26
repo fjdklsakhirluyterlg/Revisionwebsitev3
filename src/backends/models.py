73a7f2a010d0c8f56984c28d1da33534af74082c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-26 19:44:06 +0000

    stuff

--- src/backends/models.py
+++ src/backends/models.py
@@ -639,6 +639,6 @@ class ImageGuide(db.Model):
 
 class Guide(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # images = db.relationship("ImageGuide")
+    images = db.relationship("ImageGuide")
     content = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
