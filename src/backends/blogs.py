832d7f121da5127b357cc41a2b5e4e6bced96ae1
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-28 21:27:34 +0000

    stuff

--- src/backends/blogs.py
+++ src/backends/blogs.py
@@ -405,9 +405,6 @@ def blog_view_add():
 @blogs.route("/blogs/addlike/<id>")
 def add_like_to_blog(id):
     blog = db.session.query(Blog).filter(Blog.id == id).first()
-    user_id = blog.user_id
-    user = User.query.filter_by(id=user_id).first()
-    user.points += 1
     l = blog.title
     blog.likes += 1
     blog.views -= 1
@@ -417,9 +414,6 @@ def add_like_to_blog(id):
 @blogs.route("/blogs/adddislike/<id>")
 def add_dislike_to_blog(id):
     blog = db.session.query(Blog).filter(Blog.id == id).first()
-    user_id = blog.user_id
-    user = User.query.filter_by(id=user_id).first()
-    user.points -= 1
     l = blog.title
     blog.dislikes += 1
     blog.views -= 1
