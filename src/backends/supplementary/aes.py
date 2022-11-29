eb73b7e42071da715f2781a7d5bae64298ae5d5d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-29 18:17:29 +0000

    stuff

--- src/backends/supplementary/aes.py
+++ src/backends/supplementary/aes.py
@@ -15,6 +15,21 @@ class AESCipher(object):
         encrypted_text = cipher.encrypt(plain_text.encode())
         return b64encode(iv + encrypted_text).decode("utf-8")
 
-text = "hi there"
-out = AESCipher.encrypt(text)
-print(out)
+    def decrypt(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        return self.__unpad(plain_text)
+
+    def __pad(self, plain_text):
+        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        ascii_string = chr(number_of_bytes_to_pad)
+        padding_str = number_of_bytes_to_pad * ascii_string
+        padded_plain_text = plain_text + padding_str
+        return padded_plain_text
+
+    @staticmethod
+    def __unpad(plain_text):
+        last_character = plain_text[len(plain_text) - 1:]
+        return plain_text[:-ord(last_character)]
