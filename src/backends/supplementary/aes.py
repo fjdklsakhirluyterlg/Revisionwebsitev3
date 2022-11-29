961af9ebe6d5b2475d2c89256b713e526e006e49
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-11-29 18:15:06 +0000

    stuff

--- src/backends/supplementary/aes.py
+++ src/backends/supplementary/aes.py
@@ -3,3 +3,14 @@ from Crypto import Random
 from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 
+class AESCipher(object):
+    def __init__(self, key):
+        self.block_size = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, plain_text):
+        plain_text = self.__pad(plain_text)
+        iv = Random.new().read(self.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(iv + encrypted_text).decode("utf-8")
