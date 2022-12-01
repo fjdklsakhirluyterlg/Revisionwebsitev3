fe3d7f310292a40783fd277f97c1773666af24bd
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-01 19:37:27 +0000

    stuff

--- src/backends/stastics/__init__.py
+++ src/backends/stastics/__init__.py
@@ -1,3 +1,10 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from backends import create_app
 
+def new_app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    
