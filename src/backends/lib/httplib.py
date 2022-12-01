15e3a24536f38722e55510df6f9f1fe9795c77d9
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-01 18:31:47 +0000

    stuff

--- src/backends/lib/httplib.py
+++ src/backends/lib/httplib.py
@@ -10,10 +10,12 @@ def get(url: str, method):
     else:
         conn.request(method, "/")
     r1 = conn.getresponse()
-    return r1.status, r1.reason, r1.read()
+    return r1
+
+
 
 def main_run():
-    status, reason, data = get("www.python.org/", "GET")
-    print(status, reason, data)
+    res = get("www.python.org/", "GET")
+    print(res.status)
 
 main_run()
