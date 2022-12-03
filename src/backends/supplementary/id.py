0f550e99846c9a560d4ffc8cea8e29c668d64e7e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-03 21:17:52 +0000

    stuff

--- src/backends/supplementary/id.py
+++ src/backends/supplementary/id.py
@@ -1,4 +1,10 @@
 import random
 
 def generate_random_id():
-    pass
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    result = ""
+    charactersLength = len(characters) - 1
+    for i in range(16):
+        result += characters[random.randint(0, charactersLength)]
+    
+    return result
