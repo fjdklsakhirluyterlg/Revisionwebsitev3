8d217c61f75a0c4ec9a21ab7c390625703b63822
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-03 19:19:57 +0000

    stuff

--- src/backends/guides/guide.py
+++ src/backends/guides/guide.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, render_template
-from backends.models import Guide
+from backends.models import Guide, ImageGuide
 from backends import db
+from werkzeug.utils import secure_filename
+
 guide = Blueprint("guide", __name__)
 
 @guide.route("/api/guides/add", methods=["POST"])
@@ -11,3 +13,6 @@ def add_guide():
     new = Guide(content=content, user_id=user_id)
     db.session.add(new)
     db.session.commit()
+    id = getattr(new, "id")
+    for file in request.files():
+        new_image = ImageGuide()
