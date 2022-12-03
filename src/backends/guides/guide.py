22363307bfab6a2759a86d833b8e2f2783331b41
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-03 19:16:55 +0000

    stuff

--- src/backends/guides/guide.py
+++ src/backends/guides/guide.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, request, render_template
-
+from backends.models import Guide
+from backends import db
 guide = Blueprint("guide", __name__)
 
-@guide.route("/api/guides/add")
+@guide.route("/api/guides/add", methods=["POST"])
 def add_guide():
     data = request.get_json()
+    content = data["content"]
+    user_id = data["user_id"]
+    new = Guide(content=content, user_id=user_id)
     
