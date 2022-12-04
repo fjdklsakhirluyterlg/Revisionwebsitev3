6c29a107647db72665c8aa611ff1abd82dba6b42
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-04 11:08:24 +0000

    stuff

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5090, host="0.0.0.0", processes=4, threaded=False)
+    app.run(debug=True, port=5090, host="0.0.0.0")
