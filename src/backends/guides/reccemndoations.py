5ff28d3235ea03fab51f4a6e0e63585dc3bb134b
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-07 21:38:38 +0000

    stuff

--- src/backends/guides/reccemndoations.py
+++ src/backends/guides/reccemndoations.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, request
 
 recomendation_guide = Blueprint("recomendation_guide", __name__)
+
+@recomendation_guide.route("/api/reccomend/<id>")
+def reccomnd_guide_on_id():
+    pass
