394deac8098c413b0f534b4fa2de26823688c815
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-07 21:56:06 +0000

    stuff

--- src/backends/guides/reccemndoations.py
+++ src/backends/guides/reccemndoations.py
@@ -10,5 +10,9 @@ def reccomnd_guide_on_id(id):
     dict = {}
     for tag in tags:
         guides = tag.guides
-        
+        for g in guides:
+            if dict[g.id]:
+                dict[g.id] += 1
+            else:
+                dict[g.id] = 1
 
