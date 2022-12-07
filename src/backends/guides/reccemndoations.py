08617ed04c430a590be950b0dec75e0906a51b40
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-07 21:49:07 +0000

    stuff

--- src/backends/guides/reccemndoations.py
+++ src/backends/guides/reccemndoations.py
@@ -4,5 +4,5 @@ from backends.models import Guide
 recomendation_guide = Blueprint("recomendation_guide", __name__)
 
 @recomendation_guide.route("/api/reccomend/<id>")
-def reccomnd_guide_on_id():
-    
+def reccomnd_guide_on_id(id):
+    guide = Guide.query.filter_by(id=id).first()
