34612272376dc51786be2939a00373ece509b6d4
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-08 21:26:32 +0000

    stuff

--- src/backends/shop.py
+++ src/backends/shop.py
@@ -422,7 +422,7 @@ def reccomended_items(id):
     related = []
     for i in range(min, max):
         itemsx = Item.query.filter(or_(Item.price.like(f"%{i / 100}%")))
-        related.append(itemsx.name)
+        related.append(itemsx.title)
     print(related)
     return jsonify(list)
 
