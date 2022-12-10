7f56daad67128141a73c9f4fc984256caa397888
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-10 19:53:30 +0000

    stuff

--- src/backends/guides/guide_utils.py
+++ src/backends/guides/guide_utils.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, request
 
 guide_utils = Blueprint("guide_utils", __name__)
+
+@guide_utils.route("/api/guides/fork", methods=["POST"])
+def fork_guide_thing():
+    pass
