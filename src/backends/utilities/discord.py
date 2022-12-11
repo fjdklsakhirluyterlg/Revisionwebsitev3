445ba895650cd2c73fa77444cd7d119134056cd1
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-11 17:08:04 +0000

    stuff

---
+++ src/backends/utilities/discord.py
@@ -0,0 +1,17 @@
+import requests
+
+URL = "https://discord.com/api/webhooks/1051545491227299910/DHacX3fNuTkNwypJWU9vnfpSlUztwc-F9Pky-3W21sRDsKOrYHmvmxt5TKp6p5B8otS2"
+
+data = {
+    "content" : "message content",
+    "username" : "custom username"
+}
+
+result = requests.post(URL, json = data)
+
+try:
+    result.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+else:
+    print("Payload delivered successfully, code {}.".format(result.status_code))
