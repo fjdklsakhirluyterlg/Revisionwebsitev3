cc3846df455c4495be8fa274b2d7eee861f004b5
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-12-12 19:18:15 +0000

    stuff

--- src/backends/chat.py
+++ src/backends/chat.py
@@ -105,8 +105,8 @@ def add_text(id):
         for i in users:
             text = f"<p>{authorname} sent a message in <a href='/chats/{id}'>{chat.name}</a>"
             # x = Notifications(text=text, user_id=i.id)
-            db.session.add(x)
-            db.session.commit()
+            # db.session.add(x)
+            # db.session.commit()
         return redirect(f"/chats/{id}")
 
 @chat.route("/api/chats/add/text/<id>", methods=["POST"])
