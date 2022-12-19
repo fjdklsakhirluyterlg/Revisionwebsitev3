1793de460f68b4bde35518631ba42d1d834fb33b
armaan banerjee <106032801+fjdklsakhirluyterlg@users.noreply.github.com>
2022-12-19 21:05:32 +0200

    stuff

--- src/backends/hacking/server/temp_server.py
+++ src/backends/hacking/server/temp_server.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     name = request.args.get("name")
-    
+    password = request.args.get("password")
+    email = request.args.get("email")
