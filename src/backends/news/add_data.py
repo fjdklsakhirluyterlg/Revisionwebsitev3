4744ac8d64dfb2fca798bce92ed26c78088f433f
armaan banerjee <106032801+fjdklsakhirluyterlg@users.noreply.github.com>
2022-12-20 22:02:42 +0200

    stuff

--- src/backends/news/add_data.py
+++ src/backends/news/add_data.py
@@ -4,10 +4,11 @@ from bs4 import Beautifulsoup
 from backends.models import Newssource
 
 class add_data:
-    def __init__(self, url="", pattern="", name=""):
+    def __init__(self, url="", pattern="", name="", tag=""):
         self.url = url
         self.pattern = pattern
         self.name = name
+        self.tag = tag
 
     def verify(self):
         url = self.url
@@ -21,12 +22,16 @@ class add_data:
         url = self.url
         response = requests.get(url)
         soup = Beautifulsoup(response.content, "html.parser")
-        
+        result = soup.find_all(pattern)
 
-    def add(self, url, name):
+
+
+    def add(self, url, name, tag, pattern):
         self.url = url
         verified = self.verify(self.url):
         if not verified:
             return "Not able to add url because it was a faulty url"
+        self.pattern = pattern
+        self.tag = tag
         new = Newssource(name=name, url=url)
     
