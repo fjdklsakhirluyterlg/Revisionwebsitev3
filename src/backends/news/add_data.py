fbc0fd09e76e9aa813b035f48c63252722652d0e
armaan banerjee <106032801+fjdklsakhirluyterlg@users.noreply.github.com>
2022-12-20 21:48:47 +0200

    stuff

--- src/backends/news/add_data.py
+++ src/backends/news/add_data.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, request
+import requests
+
 
 class add_data:
     def __init__(self, url):
         self.url = url
 
     def verify(self):
-        pass
+        url = self.url
+        response = requests.get(url)
+        if 200 <= response.status_code <= 299 and len(response.data) > 0:
+            return True
+        return False
+
+    
