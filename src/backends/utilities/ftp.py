eebeea8fe89ce38aeba651cdc0b1c9022eabd3e7
armaan banerjee <106032801+fjdklsakhirluyterlg@users.noreply.github.com>
2022-12-22 22:03:41 +0200

    stuff

--- src/backends/utilities/ftp.py
+++ src/backends/utilities/ftp.py
@@ -1,2 +1,21 @@
 import socket
 
+sock = socket.socket()  
+host = socket.gethostname()  
+port = 12312
+
+sock.connect((host, port))
+sock.send(b"Hello server!")
+
+with open("Received_file", "wb") as out_file:
+    print("File opened")
+    print("Receiving data...")
+    while True:
+        data = sock.recv(1024)
+        print(f"{data = }")
+        if not data:
+            break
+        out_file.write(data)  # Write data to a file
+
+print("Successfully got the file")
+sock.close()
