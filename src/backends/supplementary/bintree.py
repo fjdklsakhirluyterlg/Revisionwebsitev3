153e5c5e7b77c387755660239b8359a035892e2a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-01-03 22:23:24 +0000

    stuff

--- src/backends/supplementary/bintree.py
+++ src/backends/supplementary/bintree.py
@@ -1,6 +1,12 @@
 class Treenode:
-    def __init__(self, right, left):
+    def __init__(self, right, left, value):
         self.right = right
         self.left = left
+        self.value = value
+    
+    def insert(self, value):
+        if self.value == value:
+            return 
+
     
     
