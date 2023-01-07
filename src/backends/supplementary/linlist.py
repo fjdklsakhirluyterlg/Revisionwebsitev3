d911766fb1c24403414f75ed35daad927fe614a8
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-01-07 22:50:56 +0000

    stuff

---
+++ src/backends/supplementary/linlist.py
@@ -0,0 +1,11 @@
+class Node:
+
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+    def setData(self,data):
+        self.data = data
+
+    def getData(self)
+        return self.data
