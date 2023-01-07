9216d3716753164939c74c10cbd8c87c093691d0
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-01-07 22:51:58 +0000

    stuff

--- src/backends/supplementary/linlist.py
+++ src/backends/supplementary/linlist.py
@@ -13,3 +13,14 @@ class Node:
 class LinkedList:
     def __init__(self, head = None):
         self.head = None
+    
+    def __len__(self):
+        if(self.head == None):
+            return 0
+        else:
+            count = 0
+            node = self.head
+            while(node != None):
+                node = node.next
+                count += 1
+            return count
